fc26842080e9e289303917e42922aca4e5d103ff
puneet <pvashist09@yahoo.com>
2017-02-07 13:30:54 +0530

    second commit

---
+++ src/main/java/taskForRepository/NumberMultiplication.java
@@ -0,0 +1,11 @@
+package taskForRepository;
+
+public class NumberMultiplication {
+	
+	public static void main(String an[]) {
+		
+		int a = 4,b = 5;
+		System.out.println("The product of "+a+" and "+b+" is:"+(a*b));
+	}
+
+}
