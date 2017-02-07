350939f02f77dbb36291476394c5c54ff545f4c7
puneet <pvashist09@yahoo.com>
2017-02-07 13:18:38 +0530

    initial commit

---
+++ src/main/java/taskForRepository/NumberManipulation.java
@@ -0,0 +1,12 @@
+package taskForRepository;
+
+public class NumberManipulation {
+	
+	public static void main(String an[]) {
+		
+		int a = 3, b = 4;
+		
+		System.out.println("The sum of +"+a+" and "+b+" is:"+(a+b));
+	}
+
+}
